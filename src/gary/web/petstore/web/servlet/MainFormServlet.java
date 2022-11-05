7f6a3c5e28cd79dc257dd100165816bd44e3d273
GaryFan <2985708292@qq.com>
2022-11-05 16:54:23 +0800

    init project

--- src/gary/web/petstore/web/servlet/MainFormServlet.java
+++ src/gary/web/petstore/web/servlet/MainFormServlet.java
@@ -1,6 +1,17 @@
 package gary.web.petstore.web.servlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MainFormServlet extends HttpServlet {
+
+    private static final String MAIN_FORM = "/WEB-INF/jsp/catalog/main.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(MAIN_FORM).forward(req,resp);
+    }
 }
